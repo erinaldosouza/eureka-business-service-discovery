546c22dacec251da6b60d5bd69cc5f4b91632ee2
t_francisco <t_francisco@MATRIZ-300681.hapvida.com.br>
2019-03-22 16:45:01 -0300

    First commit

---
+++ src/test/java/br/com/tcc/business/service/discovery/eurekabusinessservicediscovery/EurekaBusinessServiceDiscoveryApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.tcc.business.service.discovery.eurekabusinessservicediscovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaBusinessServiceDiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
