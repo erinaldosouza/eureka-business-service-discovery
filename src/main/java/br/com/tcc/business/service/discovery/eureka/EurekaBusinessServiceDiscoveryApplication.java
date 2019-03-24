fef280085ae78c31f014b1a47d8485de5979afff
Erina <erinaldo.sds@gmail.com>
2019-03-23 21:09:50 -0300

    eureka server configured to replication

--- src/main/java/br/com/tcc/business/service/discovery/eureka/EurekaBusinessServiceDiscoveryApplication.java
+++ src/main/java/br/com/tcc/business/service/discovery/eureka/EurekaBusinessServiceDiscoveryApplication.java
@@ -1,11 +1,15 @@
 package br.com.tcc.business.service.discovery.eureka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaClient
 @EnableEurekaServer
 @SpringBootApplication
+@EnableAutoConfiguration
 public class EurekaBusinessServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
